e45462ccb89d428575c52df6d23b124299893e76
Sam2022moon <104721736+Sam2022moon@users.noreply.github.com>
2024-04-19 12:31:48 +0900

    changed structure

--- Portfolio/resources/js/app.js
+++ Portfolio/resources/js/app.js
@@ -46,5 +46,14 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "none") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  }
+
 
 
